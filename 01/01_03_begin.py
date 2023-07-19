dccc2f47638e4e4696f22ab334e3133bd07a9360
Ronnie Sheer <ronysheer@gmail.com>
2023-07-19 17:15:29 +0300

    add: 01_02

---
+++ 01/01_03_begin.py
@@ -0,0 +1,36 @@
+
+import random
+from string import punctuation
+from collections import defaultdict
+
+
+class MarkovChain:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def _tokenize(self, text):
+        return (
+            text.translate(str.maketrans("", "", punctuation + "1234567890"))
+            .replace("\n", " ")
+            .split(" ")
+        )
+
+    def train(self, text):
+        tokens = self._tokenize(text)
+               
+
+    def generate(self, prompt, length=10):
+        # get the lask token from the prompt
+        current = self._tokenize(prompt)[-1]
+        # initialize the output
+        output = prompt
+        for i in range(length):
+            # look up the options in the graph dictionary
+            options = self.graph.get(current, [])
+            if not options:
+                continue
+            # use random.choice method to pick a current option
+            
+            # add the random choice to the output string
+    
+        return output
